469f5da0e57228eb6c6ccd0ec101198ffa4725ef
Hwiet <42421547+Hwiet@users.noreply.github.com>
2024-08-13 21:37:02 -0400

    Add example

---
+++ Pages/Home.razor.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Microsoft.JSInterop.Implementation;
+
+namespace FluentInputFileDemo.Pages;
+
+public partial class Home : IAsyncDisposable
+{
+    [Inject]
+    private IJSRuntime JSRuntime { get; set; } = default!;
+
+    [Inject]
+    private NavigationManager NavigationManager { get; set; } = default!;
+
+    private IJSObjectReference? Module { get; set; }
+
+    private FluentInputFileEventArgs? File { get; set; }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            Module ??= await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./Pages/Home.razor.js");
+        }
+    }
+
+    private void OnUpload(FluentInputFileEventArgs args)
+    {
+        File = args;
+    }
+
+    // It would be better to show a thumbnail preview for files, but
+    // allow users to download the uploaded file as an alternative to
+    // previewing the file on the same webpage.
+    private async Task DownloadFileAsync()
+    {
+        if (File != null && Module != null)
+        {
+            var fileStream = new MemoryStream(File.Buffer.Data);
+            using var streamRef = new DotNetStreamReference(fileStream);
+
+            await Module.InvokeVoidAsync("downloadFileFromStream", File.Name, streamRef);
+        }
+    }
+
+    private void ClearFile()
+    {
+        File = null;
+    }
+
+    async ValueTask IAsyncDisposable.DisposeAsync()
+    {
+        if (Module != null)
+        {
+            await Module.DisposeAsync();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Pages/Home.razor.js
@@ -0,0 +1,13 @@
+﻿// From "ASP.NET Core Blazor file downloads: Download from a stream" documentation page
+// https://learn.microsoft.com/en-us/aspnet/core/blazor/file-downloads?view=aspnetcore-8.0#download-from-a-stream
+export async function downloadFileFromStream(fileName, contentStreamReference) {
+  const arrayBuffer = await contentStreamReference.arrayBuffer();
+  const blob = new Blob([arrayBuffer]);
+  const url = URL.createObjectURL(blob);
+  const anchorElement = document.createElement('a');
+  anchorElement.href = url;
+  anchorElement.download = fileName ?? '';
+  anchorElement.click();
+  anchorElement.remove();
+  URL.revokeObjectURL(url);
+}
